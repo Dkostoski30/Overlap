c429dd7cdae510554163d0460cd5306d54880ef4
Dragana Usovic <draganausovic2@gmail.com>
2025-06-18 12:15:29 +0200

    Add lap and checkpoint prefab

--- Overlap/Assets/Scripts/CarInputHandler.cs
+++ Overlap/Assets/Scripts/CarInputHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CarInputHandler : MonoBehaviour
 {
+    public int playerNumber = 1;
+
     TopDownCarController topDownCarController;
 
     void Awake()
@@ -20,8 +22,18 @@ public class CarInputHandler : MonoBehaviour
     {
         Vector2 inputVector = Vector2.zero;
 
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        switch (playerNumber)
+        {
+            case 1:
+                inputVector.x = Input.GetAxis("Horizontal_P1");
+                inputVector.y = Input.GetAxis("Vertical_P1");
+                break;
+
+            case 2:
+                inputVector.x = Input.GetAxis("Horizontal_P2");
+                inputVector.y = Input.GetAxis("Vertical_P2");
+                break;
+        }
 
         topDownCarController.SetInputVector(inputVector);
     }
